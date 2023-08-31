1b627b33a91d8a0f6a9940d23f0c90a7e6955f0c
balart40 <ng711690@iteso.mx>
2023-08-30 22:35:42 -0700

    adding acm certificate

---
+++ acm-certificate.tf
@@ -0,0 +1,17 @@
+module "acm" {
+  source  = "terraform-aws-modules/acm/aws"
+
+  domain_name  = "my-domain.com"
+  zone_id      = "Z2ES7B9AZ6SHAE"
+
+  subject_alternative_names = [
+    "*.franciscobalart.io",
+    "franciscobalart.io",
+  ]
+
+  wait_for_validation = true
+
+  tags = {
+    Name = "franciscobalart.io"
+  }
+}
