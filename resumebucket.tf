28da50a5ea3cf63a189fd1486bc714004af2f6c3
balart40 <ng711690@iteso.mx>
2023-08-30 21:53:47 -0700

    changind s3 bucket

--- resumebucket.tf
+++ resumebucket.tf
@@ -2,6 +2,32 @@ resource "aws_s3_bucket" "example" {
   bucket = "franciscobalart.io"
 }
 
+resource "aws_s3_bucket_ownership_controls" "example" {
+  bucket = aws_s3_bucket.example.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.example.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.example,
+    aws_s3_bucket_public_access_block.example,
+  ]
+
+  bucket = aws_s3_bucket.example.id
+  acl    = "public-read"
+}
+
 resource "aws_s3_bucket_policy" "public_read_policy" {
   bucket = "franciscobalart.io"
 
